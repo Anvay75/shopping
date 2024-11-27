8a982692758395f135736684a277887019de3be4
Anvay75 <anvaz2880@gmail.com>
2024-11-27 16:34:28 +0100

    s.py

--- s.py
+++ s.py
@@ -1,32 +1,37 @@
 import random
 print("welcome to the general shop \n \n what would you like to buy")
+
 clothes = 0
 fruits = 0
 medicines = 0
-choice = 0
 total = 0
 while True:
-    
+    choice = 0
+    choice1 = 0
+    choice2 = 0
     print("1.clothes \n 2.fruits \n 3.medicines")
     yeah = int(input("what would you like to buy :"))
     if yeah == 1:
         clothes = int(input("how many do you want :"))
-        choice = clothes * random.randint(10,40)
-        total += choice
+        choice = clothes * random.randint(10,40) 
+        total += choice 
+        print("the price of this is :",choice)
     
     elif yeah == 2:
         fruits = int(input("how many do you want :"))
-        choice = fruits * random.randint(2,15)
-        total += choice
-     
+        choice1 = fruits * random.randint(2,15)
+        total += choice1
+        print("the price of this is :",choice1)
+    
     elif yeah == 3:
         medicines = int(input("how many do you want :"))
-        choice = medicines * random.randint(10,40)
-        total += choice
-
-    print("price = ",total)
+        choice2 = medicines * random.randint(10,40)
+        total += choice2
+        print("the price of this is:",choice2)
+        
     ble = input("do you still want to play this game (n/y) only lowercase letters work)")
     if ble == "n":
         print("your bill is :",total)
+        print("thank you for shopping with us...!")
         break
         
